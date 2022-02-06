86f70b4bf2ef0e5cf1beea515369d5a52ecc7a9f
minarsvn <minarshovon909@gmail.com>
2022-02-06 16:27:36 +0600

    stable

--- next.config.js
+++ next.config.js
@@ -1,33 +1,10 @@
-const withOffline = require('next-offline')
+const withPWA = require('next-pwa')
 
-module.exports = withOffline({
-  workboxOpts: {
-    swDest: process.env.NEXT_EXPORT
-      ? 'service-worker.js'
-      : 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'offlineCache',
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/service-worker.js',
-        destination: '/_next/static/service-worker.js',
-      },
-    ]
-  },
+module.exports = withPWA({
+    pwa: {
+        dest: 'public'
+    }
 })
-
 // next.config.js
 module.exports = {
   swcMinify: true
